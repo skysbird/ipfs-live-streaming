225a43f22f74cafe828faae7416b9d447da16fb0
伯诚 <yi.fu@alibaba-inc.com>
2022-10-06 19:12:52 +0800

    push stream

---
+++ terraform/rtmp-server/process-live.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Load settings
+
+# Create directory for HLS content
+#rm -rf ./hls/*
+#cd ./hls
+
+
+
+what="live"
+key="key"
+RESTARTED="1"
+
+
+func() {
+    echo "Usage:"
+    echo "[-i source file] [-k ipns key] [-w workdir]"
+    exit -1
+}
+ 
+upload="false"
+ 
+while getopts 'i:k:u:w:' OPT; do
+    case $OPT in
+        i) sfile="$OPTARG";;
+        k) key="$OPTARG";;
+        w) workdir="$OPTARG";;
+        ?) func;;
+    esac
+done
+
+cmd="ffmpeg -stream_loop -1 -re -i $sfile -c:v libx264 -s 720x576 -c:a copy -f hls -hls_time 10 -hls_list_size 3 $workdir/live.m3u8"
+
+$cmd 1>/dev/null 2>&1 &
+ 
+echo $key
+echo $workdir
+cd $workdir
+
+#tmpdir=$(echo -n 'some-value' | md5)
+#echo $tmpdir
+
+while true; do
+  nextfile=$(cat ${what}.m3u8 | tail -n1)
+
+  if [ -f "${nextfile}" ]; then
+    timecode=$(grep -B1 ${nextfile} ${what}.m3u8 | head -n1 | awk -F : '{print $2}' | tr -d ,)
+    if ! [ -z "${nextfile}" ]; then
+      if ! [ -z "{$timecode}" ]; then
+        reset_stream_marker=''
+        if [[ "$(grep -B2 ${nextfile} ${what}.m3u8 | head -n1)" == "#EXT-X-DISCONTINUITY" || "${RESTARTED}" == "1" ]]; then
+          reset_stream_marker=" #EXT-X-DISCONTINUITY"
+          RESTARTED="0"
+        fi
+
+        # Current UTC date for the log
+        time=$(date "+%F-%H-%M-%S")
+
+        # Add ts file to IPFS
+        hash=$(ipfs add -Q ${nextfile})
+
+        if [[ -z "${hash}" ]]; then
+          echo ${nextfile} Add Failed, skipping for retry
+        else
+          # Update the log with the future name (hash already there)
+          echo added ${hash} ${nextfile} ${time}.ts ${timecode}${reset_stream_marker} >>~/process-stream.log
+
+          # Remove nextfile and tmp.txt
+          rm -f ${nextfile} ~/tmp.txt
+          echo "#EXTM3U" >current.m3u8
+          echo "#EXT-X-VERSION:3" >>current.m3u8
+          echo "#EXT-X-TARGETDURATION:20" >>current.m3u8
+          echo "#EXT-X-MEDIA-SEQUENCE:0" >>current.m3u8
+          echo "#EXT-X-PLAYLIST-TYPE:EVENT" >>current.m3u8
+
+          cat ~/process-stream.log | awk '{print $6"#EXTINF:"$5",\n'${IPFS_GATEWAY}'/ipfs/"$2}' | sed 's/#EXT-X-DISCONTINUITY#/#EXT-X-DISCONTINUITY\n#/g' >>current.m3u8
+
+          # Add m3u8 file to IPFS and IPNS publish (uncomment to enable)
+          m3u8hash=$(ipfs add current.m3u8 | awk '{print $2}')
+          ipfs name publish --ttl 1s --timeout=5s $m3u8hash &
+          
+          # Copy files to web server
+          #cp current.m3u8 /var/www/html/live.m3u8
+          #cp ~/process-stream.log /var/www/html/live.log
+        fi
+      fi
+    fi
+  else
+    sleep 1
+  fi
+done
